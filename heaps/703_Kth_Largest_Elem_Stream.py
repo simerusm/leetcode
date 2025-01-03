3dfdb86528d909943e9c6ffd0e5343f68d7554af
Simerus Mahesh <63352151+simerusm@users.noreply.github.com>
2025-01-03 17:14:22 -0500

    Update 703_Kth_Largest_Elem_Stream.py

--- heaps/703_Kth_Largest_Elem_Stream.py
+++ heaps/703_Kth_Largest_Elem_Stream.py
@@ -1,17 +1,17 @@
 import heapq
 class KthLargest:
     def __init__(self, k: int, nums: List[int]):
-        heapq.heapify(nums)
-        while len(nums) > k:
-            heapq.heappop(nums)
-
-        self.min_heap = nums
+        self.min_heap = nums.copy()
         self.k = k
+
+        heapq.heapify(self.min_heap)
+        while len(self.min_heap) > k:
+            heapq.heappop(self.min_heap)
 
     def add(self, val: int) -> int:
         if len(self.min_heap) < self.k:
             heapq.heappush(self.min_heap, val)
-            
+
         # Only add if val to add is bigger than the kth largest element currently
         elif self.min_heap[0] < val:
             heapq.heapreplace(self.min_heap, val)
