45dd6da3b208b79fc623cea0ce63ecfc3776c09e
Simerus Mahesh <63352151+simerusm@users.noreply.github.com>
2025-01-11 14:34:14 -0500

    Update 2090_K_Radius_Subarray_Avgs.py

--- arrays_and_strings/2090_K_Radius_Subarray_Avgs.py
+++ arrays_and_strings/2090_K_Radius_Subarray_Avgs.py
@@ -1,7 +1,7 @@
 class Solution:
     def getAverages(self, nums: List[int], k: int) -> List[int]:
-        k_radius_averages = [0 for i in range(len(nums))]
-        
+        k_radius_averages = [-1 for i in range(len(nums))]
+
         prefix_sum = []
         accumulation = 0
         for num in nums:
@@ -9,7 +9,6 @@ class Solution:
             prefix_sum.append(accumulation)
 
         for i in range(len(nums)):
-            k_radius_averages[i] = -1
             if i - k >= 0 and i + k < len(nums):
                 curr_radius_sum = prefix_sum[i + k]
                 if i - k - 1 >= 0:
