5272c6f3868b905684ada7f6ded7cdf4d4ba556e
Simerus Mahesh <63352151+simerusm@users.noreply.github.com>
2025-01-11 15:44:42 -0500

    Update 2225_Find_Players_Zero_One_Losses.py

--- hashing/2225_Find_Players_Zero_One_Losses.py
+++ hashing/2225_Find_Players_Zero_One_Losses.py
@@ -2,15 +2,15 @@ from collections import defaultdict
 from bisect import insort
 class Solution:
     def findWinners(self, matches: List[List[int]]) -> List[List[int]]:
-        players = defaultdict(int) # losses
+        player_losses = defaultdict(int) # losses
 
         for winner, loser in matches:
-            players[winner] += 0
-            players[loser] += 1
+            player_losses[winner] += 0
+            player_losses[loser] += 1
         
         no_losses = []
         one_loss = []
-        for player, losses in players.items():
+        for player, losses in player_losses.items():
             if losses == 0:
                 insort(no_losses, player)
             elif losses == 1:
