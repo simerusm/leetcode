835e3f4af5b6c2c47654011c09d177fd46629e61
Simerus Mahesh <63352151+simerusm@users.noreply.github.com>
2025-02-12 21:07:04 -0500

    Update 35_Search_Insert_Pos.py

--- binary_search/35_Search_Insert_Pos.py
+++ binary_search/35_Search_Insert_Pos.py
@@ -2,3 +2,25 @@ from bisect import bisect_left
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
         return bisect_left(nums, target)
+
+
+--------------------------------------------------------------------
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        l = 0
+        r = len(nums) - 1
+
+        while l <= r:
+            m = (l + r) // 2
+            if nums[m] == target:
+                return m
+            elif nums[m] > target:
+                r -= 1
+            else:
+                l += 1
+
+        if l < 0:
+            l = 0
+        
+        return l
