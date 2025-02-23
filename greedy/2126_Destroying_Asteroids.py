c03e3a47d4bc20cf96c5001859b5390d912bacac
Simerus Mahesh <63352151+simerusm@users.noreply.github.com>
2025-02-23 13:57:34 -0500

    Update 2126_Destroying_Asteroids.py

--- greedy/2126_Destroying_Asteroids.py
+++ greedy/2126_Destroying_Asteroids.py
@@ -1,10 +1,18 @@
 class Solution:
     def asteroidsDestroyed(self, mass: int, asteroids: List[int]) -> bool:
         asteroids.sort()
-        
-        for asteroid in asteroids:
-            if asteroid > mass:
+
+        for asteroid_mass in asteroids:
+            if asteroid_mass > mass:
                 return False
-            mass += asteroid
+            mass += asteroid_mass
         
         return True
+
+"""
+- greedy algo problem
+- we need to ensure we hit the mass (planet) with the smallest asteroid masses first
+    - by doing this we gaurantee to be getting the mass of the planet as big as possible resulting in us being able to have the best attempt of destroying all the asteroid masses 
+- time complexity: O(nlogn + n) --> O(nlogn), O(nlogn) due to sorting, O(n) due to iterating through the asteroid array but overall it gets ammortized to O(nlogn)
+- space complexity: O(1) because we're modifying the array to sort in place
+"""
